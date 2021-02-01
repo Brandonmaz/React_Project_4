e99250a56cd89a4ca28781dfd950e8b1687961d5
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2021-02-01 15:36:35 -0500

    FInished task list

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -7,40 +7,43 @@ class Entry extends Component {
                 <div className="task">
                     <h4>Task: {this.props.entry.task}</h4>
                     <h4>Due Date: {this.props.entry.due_date}</h4>
-                    <details value="More Info">
+                    <details >
+                        <summary>More Info</summary>
                         <h4>Description: {this.props.entry.description}</h4>
 
 
                         {/* <input type="checkbox" {this.props.entry.done}/> */}
                         <button value={this.props.entry.id} onClick={this.props.deleteEntry}>Delete</button>
-                        <h4>Update Entry</h4>
-                        <form id={this.props.entry.id} onSubmit={this.props.updateEntry}>
-                            <label htmlFor="task">Task</label>
-                            <input
-                                type="text"
-                                id="task"
-                                onChange={this.props.handleChange}
-                                value={this.props.task}
-                            />
-                            <br />
-                            <label htmlFor="description">Description</label>
-                            <input
-                                type="text"
-                                id="description"
-                                onChange={this.props.handleChange}
-                                value={this.props.description}
-                            />
-                            <br />
-                            <label htmlFor="duedate">Due Date</label>
-                            <input
-                                type="text"
-                                id="duedate"
-                                onChange={this.props.handleChange}
-                                value={this.props.due_date}
-                            />
-                            <br />
-                            <input type="submit" value="Update Task"/>
-                        </form>
+                        <details>
+                            <summary>Update Entry</summary>
+                            <form id={this.props.entry.id} onSubmit={this.props.updateEntry}>
+                                <label htmlFor="task">Task</label>
+                                <input
+                                    type="text"
+                                    id="task"
+                                    onChange={this.props.handleChange}
+                                    value={this.props.task}
+                                />
+                                <br />
+                                <label htmlFor="description">Description</label>
+                                <input
+                                    type="text"
+                                    id="description"
+                                    onChange={this.props.handleChange}
+                                    value={this.props.description}
+                                />
+                                <br />
+                                <label htmlFor="duedate">Due Date</label>
+                                <input
+                                    type="date"
+                                    id="duedate"
+                                    onChange={this.props.handleChange}
+                                    value={this.props.due_date}
+                                />
+                                <br />
+                                <input type="submit" value="Update Task"/>
+                            </form>
+                        </details>
                     </details>
                 </div>
             </div>
